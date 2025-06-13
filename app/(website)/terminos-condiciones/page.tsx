c681b4c79e912a3f2ae35f24591f05116539af15
JuanFernandoo <jfernandoorjuela26@gmail.com>
2025-06-13 15:09:43 -0500

    DEV: Term-Condition / Form-Legal

--- app/(website)/terminos-condiciones/page.tsx
+++ app/(website)/terminos-condiciones/page.tsx
@@ -1,50 +1,315 @@
 'use client';
-import { LlianzIcon } from "@/modules/shared/components/SVGIcons";
+import { AxaIcon, BolivarIcon, HdiIcon, LibertyIcon, LlianzIcon, MapfreIcon, SbsIcon, SolidariaIcon, SuraIcon } from "@/modules/shared/components/SVGIcons";
+import { AttendanceItem, DocumentItem } from "@/modules/shared/types/termConditions";
 import Wrapper from "@/modules/shared/ui/Wrapper";
-import { getAssetPath } from "@/modules/shared/utils/paths";
-import { ATTENDANCES } from "@/modules/website/data/attendances";
-import Image from "next/image";
-import { useState } from "react";
-
+import React, { JSX, useState } from "react";
+export const ATTENDANCES: AttendanceItem[] = [
+  {
+    id: 0,
+    icon: <LlianzIcon />,
+    document: [
+      {
+        idDocument: 1,
+        title: "Pólizas de Carros y Motos",
+        url: "/"
+      },
+      {
+        idDocument: 2,
+        title: "Pólizas de Mascotas",
+        url: "/"
+      },
+      {
+        idDocument: 3,
+        title: "Pólizas de Bicis",
+        url: "/"
+      },
+      {
+        idDocument: 4,
+        title: "Pólizas de Vida",
+        url: "/"
+      },
+      {
+        idDocument: 5,
+        title: "Pólizas de Hogar",
+        url: "/"
+      },
+      {
+        idDocument: 6,
+        title: "Pólizas de Exequias",
+        url: "/"
+      }
+    ]
+  },
+  {
+    id: 1,
+    icon: <AxaIcon />,
+    document: [
+      {
+        idDocument: 1,
+        title: "Taxis",
+        url: "/"
+      },
+      {
+        idDocument: 2,
+        title: "Pesados/Públicos",
+        url: "/"
+      },
+      {
+        idDocument: 3,
+        title: "Motos",
+        url: "/"
+      },
+      {
+        idDocument: 4,
+        title: "Particular",
+        url: "/"
+      }
+    ]
+  },
+  {
+    id: 2,
+    icon: <HdiIcon />,
+    document: [
+      {
+        idDocument: 1,
+        title: "Pólizas de Carros y Motos",
+        url: "/"
+      },
+      {
+        idDocument: 2,
+        title: "Pólizas de Mascotas",
+        url: "/"
+      },
+      {
+        idDocument: 3,
+        title: "Pólizas de Bicis",
+        url: "/"
+      },
+      {
+        idDocument: 4,
+        title: "Pólizas de Vida",
+        url: "/"
+      },
+      {
+        idDocument: 5,
+        title: "Pólizas de Hogar",
+        url: "/"
+      },
+      {
+        idDocument: 6,
+        title: "Pólizas de Exequias",
+        url: "/"
+      }
+    ]
+  },
+  {
+    id: 3,
+    icon: <LibertyIcon />,
+    document: [
+      {
+        idDocument: 1,
+        title: "Taxis",
+        url: "/"
+      },
+      {
+        idDocument: 2,
+        title: "Pesados/Públicos",
+        url: "/"
+      },
+      {
+        idDocument: 3,
+        title: "Motos",
+        url: "/"
+      },
+      {
+        idDocument: 4,
+        title: "Particular",
+        url: "/"
+      }
+    ]
+  },
+  {
+    id: 4,
+    icon: <MapfreIcon />,
+    document: [
+      {
+        idDocument: 1,
+        title: "Taxis",
+        url: "/"
+      },
+      {
+        idDocument: 2,
+        title: "Pesados/Públicos",
+        url: "/"
+      },
+      {
+        idDocument: 3,
+        title: "Motos",
+        url: "/"
+      },
+      {
+        idDocument: 4,
+        title: "Particular",
+        url: "/"
+      }
+    ]
+  },
+  {
+    id: 5,
+    icon: <SbsIcon />,
+    document: [
+      {
+        idDocument: 1,
+        title: "Taxis",
+        url: "/"
+      },
+      {
+        idDocument: 2,
+        title: "Pesados/Públicos",
+        url: "/"
+      },
+      {
+        idDocument: 3,
+        title: "Motos",
+        url: "/"
+      },
+      {
+        idDocument: 4,
+        title: "Particular",
+        url: "/"
+      }
+    ]
+  },
+  {
+    id: 6,
+    icon: <BolivarIcon />,
+    document: [
+      {
+        idDocument: 1,
+        title: "Taxis",
+        url: "/"
+      },
+      {
+        idDocument: 2,
+        title: "Pesados/Públicos",
+        url: "/"
+      },
+      {
+        idDocument: 3,
+        title: "Motos",
+        url: "/"
+      },
+      {
+        idDocument: 4,
+        title: "Particular",
+        url: "/"
+      }
+    ]
+  },
+  {
+    id: 7,
+    icon: <SolidariaIcon />,
+    document: [
+      {
+        idDocument: 1,
+        title: "Pólizas de Carros y Motos",
+        url: "/"
+      },
+      {
+        idDocument: 2,
+        title: "Pólizas de Mascotas",
+        url: "/"
+      },
+      {
+        idDocument: 3,
+        title: "Pólizas de Bicis",
+        url: "/"
+      },
+      {
+        idDocument: 4,
+        title: "Pólizas de Vida",
+        url: "/"
+      },
+      {
+        idDocument: 5,
+        title: "Pólizas de Hogar",
+        url: "/"
+      },
+      {
+        idDocument: 6,
+        title: "Pólizas de Exequias",
+        url: "/"
+      }
+    ]
+  },
+  {
+    id: 8,
+    icon: <SuraIcon />,
+    document: [
+      {
+        idDocument: 1,
+        title: "Taxis",
+        url: "/"
+      },
+      {
+        idDocument: 2,
+        title: "Pesados/Públicos",
+        url: "/"
+      },
+      {
+        idDocument: 3,
+        title: "Motos",
+        url: "/"
+      },
+      {
+        idDocument: 4,
+        title: "Particular",
+        url: "/"
+      }
+    ]
+  },
+]
 const ButtonOption: React.FC<{
-  active: boolean
-}> = ({ active }) => {
+  active: boolean,
+  icon: React.ReactNode
+  onClick: () => void
+}> = ({ active, icon, onClick }) => {
   return (
     <button
+      onClick={onClick}
       className={`h-[82px] w-full rounded-2xl flex justify-center items-center overflow-hidden cursor-pointer p-5 ${active ? "bg-yellow-primary text-white" : "border border-blue-terciary bg-white text-gray-4"}`}
     >
-      <LlianzIcon />
+      {icon}
     </button>
   )
 }
-
-const TermCard: React.FC = () => {
+type TermCardProps = {
+  document: DocumentItem
+  icons: JSX.Element
+}
+const TermCard: React.FC<TermCardProps> = ({ document, icons }) => {
   return (
-    <div className="rounded-[30px] overflow-hidden bg-gray-3 flex flex-col cursor-pointer">
-      <div className="flex justify-center items-center flex-1">
-        <Image
-          src={getAssetPath("/icons/allianz.svg")}
-          alt="Imágen de aseguradora"
-          width={150}
-          height={60}
-          style={{ height: 'auto' }}
-        />
+    <div className="w-[348px] h-[368px] rounded-[30px] overflow-hidden flex flex-col bg-[#F1F4FB] shadow-sm cursor-pointer">
+      <div className="bg-[#F1F4FB] flex flex-col justify-center items-center flex-1 px-4 py-10justify-center">
+        <div className="w-[150px] h-[38px] flex justify-center items-center text-[#A1A2A1]">
+          {icons}
+        </div>
       </div>
-      <div className="flex items-center gap-8 px-10 py-6 bg-blue-terciary">
-        <h5 className="text-3xl text-center text-blue-primary font-bold">Póliza de carros y motos</h5>
+      <div className="bg-[#D9E9FB] h-[126px] px-11 flex justify-center items-center">
+        <a href={document.url} target="_blank" className="w-full text-center">
+          <h5 className="text-3xl font-bold text-[#345BB0] leading-tight">
+            {document.title}
+          </h5>
+        </a>
       </div>
     </div>
   )
 }
-
 const TermsConditions: React.FC = () => {
-
-  const [index] = useState(0);
-
+  const [index, setIndex] = useState<number>(0);
   return (
     <div className="relative">
       <div className="h-[281px] w-full bg-blue-terciary absolute top-0 left-0"></div>
-
       <Wrapper>
         <div className="relative grid grid-cols-[221px_1fr] gap-14 py-10 pb-[70px]">
           <div className="space-y-4">
@@ -52,6 +317,8 @@ const TermsConditions: React.FC = () => {
               <ButtonOption
                 key={i}
                 active={index == i}
+                icon={a.icon}
+                onClick={() => setIndex(i)}
               />
             ))}
           </div>
@@ -60,9 +327,13 @@ const TermsConditions: React.FC = () => {
               <h1 className="text-3xl text-blue-primary font-bold">Términos y Condiciones</h1>
               <p className="text-[20px] text-text-3 font-medium">Selecciona la categoría deseada.</p>
             </div>
-            <div className="grid grid-cols-[repeat(3,348px)] auto-rows-[348px] gap-4">
-              {Array.from({ length: 6 }).map((_, i) => (
-                <TermCard key={i} />
+            <div className="grid lg:grid-cols-[repeat(3,348px)] auto-cols-[348px] gap-4">
+              {ATTENDANCES[index].document.map((doc) => (
+                <TermCard
+                  key={doc.idDocument}
+                  document={doc}
+                  icons={ATTENDANCES[index].icon}
+                />
               ))}
             </div>
           </div>
@@ -71,5 +342,4 @@ const TermsConditions: React.FC = () => {
     </div>
   )
 }
-
 export default TermsConditions;
